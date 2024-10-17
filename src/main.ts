d21a9f393e084103fda8d57369ad67bba3868bc6
WontonsofDMG <150846431+WontonsofDMG@users.noreply.github.com>
2024-10-17 20:00:56 +0000

    Step 1: Layout completed

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,7 @@ const APP_NAME = "Hello";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+app.innerHTML = `
+  <h1>${APP_NAME}</h1>
+  <canvas id="myCanvas" width="256" height="256"></canvas>
+`;
