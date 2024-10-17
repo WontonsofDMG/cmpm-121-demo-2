b9454b4feb14660e2756ed75ee5df4de4165f59b
WontonsofDMG <150846431+WontonsofDMG@users.noreply.github.com>
2024-10-17 20:04:04 +0000

    step 2: Simple Marker completed

--- src/main.ts
+++ src/main.ts
@@ -7,4 +7,34 @@ document.title = APP_NAME;
 app.innerHTML = `
   <h1>${APP_NAME}</h1>
   <canvas id="myCanvas" width="256" height="256"></canvas>
+  <button id="clearButton">Clear</button>
 `;
+
+const canvas = document.querySelector<HTMLCanvasElement>("#myCanvas")!;
+const ctx = canvas.getContext("2d")!;
+let drawing = false;
+
+canvas.addEventListener("mousedown", () => {
+  drawing = true;
+});
+
+canvas.addEventListener("mouseup", () => {
+  drawing = false;
+  ctx.beginPath();
+});
+
+canvas.addEventListener("mousemove", (event) => {
+  if (!drawing) return;
+  ctx.lineWidth = 2;
+  ctx.lineCap = "round";
+  ctx.strokeStyle = "black";
+
+  ctx.lineTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.moveTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
+});
+
+document.querySelector<HTMLButtonElement>("#clearButton")!.addEventListener("click", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
